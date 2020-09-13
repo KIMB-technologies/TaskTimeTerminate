f64f96d8f421733313c928b7d671a007a0c038c4
KIMB-technologies <info@kimb-technologies.eu>
2020-09-13 22:36:49 +0200

    Some changes to MacDialog

--- core/platform/macos/TaskTimeTerminate/TaskTimeTerminate/ViewController.swift
+++ core/platform/macos/TaskTimeTerminate/TaskTimeTerminate/ViewController.swift
@@ -16,14 +16,34 @@ extension FileHandle : TextOutputStream {
 
 class ViewController: NSViewController {
     
+    /**
+     * GUI References
+     */
     @IBOutlet weak var categoryDropdown: NSPopUpButtonCell!
     
     @IBOutlet weak var nameInput: NSTextField!
     
     @IBOutlet weak var timeInput: NSTextField!
     
+    /**
+     * Enter Handling on Textareas
+     */
+    @IBAction func taskFieldEnter(_ sender: NSTextField) {
+        if( timeInput.stringValue != "" && nameInput.stringValue != "" ){
+            startClicked(nil);
+        }
+    }
+    @IBAction func timeFieldEnter(_ sender: NSTextField) {
+        if( timeInput.stringValue != "" ){
+            startClicked(nil);
+        }
+    }
     
-    @IBAction func startClicked(_ sender: NSButton) {
+    
+    /**
+     * Send to PHP
+     */
+    @IBAction func startClicked(_ sender: NSButton?) {
         var output = "{ \"pause\" : false, ";
         output += " \"cat\": \"" + (categoryDropdown.titleOfSelectedItem ?? "").replacingOccurrences(of: "\"", with: "") + "\", ";
         output += " \"name\": \"" + nameInput.stringValue.replacingOccurrences(of: "\"", with: "") + "\", ";
@@ -36,15 +56,18 @@ class ViewController: NSViewController {
         NSApplication.shared.terminate(self);
     }
     
-    @IBAction func pauseClicked(_ sender: NSButton) {
+    @IBAction func pauseClicked(_ sender: NSButton?) {
         var stdOut = FileHandle.standardOutput;
         print("{ \"pause\" : true, \"cat\": \"\", \"name\": \"\", \"time\": \"\" }", to:&stdOut);
         
         NSApplication.shared.terminate(self);
     }
     
+    /**
+     * On load setup, load args
+     */
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
         
         var catList = ["No", "Categories", "given!", "Use", "argument", "-cats Test,TTT,UUU"];
         if(CommandLine.arguments.count == 3 && CommandLine.arguments[1] == "-cats" ){
@@ -55,6 +78,9 @@ class ViewController: NSViewController {
         categoryDropdown.addItems(withTitles: catList);
     }
     
+    /**
+     * Stay in foreground
+     */
     override func viewWillAppear() {
         NSApplication.shared.activate(ignoringOtherApps: true);
         view.window?.level = .floating;

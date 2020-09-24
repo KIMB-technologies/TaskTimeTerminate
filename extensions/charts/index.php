709c8dac2ae743b7bda00aacd8a322788b343c7b
KIMB-technologies <info@kimb-technologies.eu>
2020-09-24 14:52:49 +0200

    Fix Error in Charts Extension

--- extensions/charts/index.php
+++ extensions/charts/index.php
@@ -69,12 +69,12 @@ class Charts {
 		$c = str_repeat("-", self::LINE_LENGTH+1) . PHP_EOL;
 		$fullLength = 0;
 		foreach($lengths as $name => $d){
-			if( $d > 0){
+			if( $d > 0 ){
 				$c .= '|' . \str_pad($name, $d - 1, " ", STR_PAD_BOTH);
 				$fullLength += $d < 1 ? 1 : $d;
 			}
 		}
-		$c .= str_repeat(" ", self::LINE_LENGTH - $fullLength) . '|' . PHP_EOL;
+		$c .= ( self::LINE_LENGTH - $fullLength > 0 ? str_repeat(" ", self::LINE_LENGTH - $fullLength) : '') . '|' . PHP_EOL;
 		$c .= str_repeat("-", self::LINE_LENGTH+1);
 		return $c;
 	}
